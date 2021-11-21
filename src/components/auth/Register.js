d18beea60d0b021813ceeb75ed2742aa10f2934a
Harit Joshi <joshiharit3@gmail.com>
2021-11-21 16:46:31 +0530

    signup flow fix-1

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -79,7 +79,7 @@ class Register extends React.Component {
   };
 
   displayErrors = (errors) =>
-    errors.map((error, i) => <p key={i}>{error.message}</p>);
+    errors.map((error, i) => <p key={i}>{error.message.split("Firebase:")[1]}</p>);
 
   handleChange = (event) => {
     this.setState({ [event.target.name]: event.target.value });
